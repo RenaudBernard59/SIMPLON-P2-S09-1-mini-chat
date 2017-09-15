72967fade2efcff4eb6cfbb299f27c77f1d74224
RenaudBernard59 <renaud.bernard@p59production.com>
2017-09-15 10:38:40 +0200

    Adding form to index.php NOT TESTED

--- index.php
+++ index.php
@@ -5,6 +5,31 @@
 	<title>blah</title>
 </head>
 <body>
-	<p>Content</p>
+	<header><h1>Le petit chat</h1></header>
+	<main>
+	<section>
+		<p>Hello</p>
+		<p>Hello</p>
+		<p>Hello</p>
+		<p>Hello</p>
+		<p>Hello</p>
+		<p>Hello</p>
+	</section>
+	<hr/>
+	<section>
+		<form action="cible.php" methode="post">
+			<label for="pseudo">Votre pseudo :</label>
+			<input type="text" name="pseudo" id="psudo" />	
+	
+			<br/>
+
+			<label for="message">Votre message :</label>
+			<textarea type="text" name="message" id="message"></textarea>			
+
+			<input type="submit" value="Envoyer message" />
+		</form>
+	</section>
+	</main>
+	<footer><small>&copy; 2017 - Renaud BERNARD</small></footer>
 </body>
 </html>
