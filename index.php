2a68dedca9d667dfa69bb9a85edf938550a8d8cf
RenaudBernard59 <renaud.bernard@p59production.com>
2017-09-18 12:13:01 +0200

    Adding datetime in hidden form + SQL Request inside commentaries

--- index.php
+++ index.php
@@ -3,6 +3,22 @@
 <head>
 	<meta charset=utf-8>
 	<title>Le petit chat</title>
+	<script>
+
+		function js_yyyy_mm_dd_hh_mm_ss() {
+		  now = new Date();
+		  year = "" + now.getFullYear();
+		  month = "" + (now.getMonth() + 1); if (month.length == 1) { month = "0" + month; }
+		  day = "" + now.getDate(); if (day.length == 1) { day = "0" + day; }
+		  hour = "" + now.getHours(); if (hour.length == 1) { hour = "0" + hour; }
+		  minute = "" + now.getMinutes(); if (minute.length == 1) { minute = "0" + minute; }
+		  second = "" + now.getSeconds(); if (second.length == 1) { second = "0" + second; }
+		  return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+		}
+
+		document.getElementById("datetime").value = js_yyyy_mm_dd_hh_mm_ss();
+
+	</script>
 </head>
 <body>
 	<header><h1>Le petit chat</h1></header>
@@ -20,6 +36,12 @@ Si le pseudo existe afficher Le chat [SECTION1]
 while (i machin) {
 	echo "<p>" . [SQL] . "</p>";
 
+	/*
+	Requette SQL pour tout enregistrer
+	[SELECT utilisateurschat.nomutilisateur, messageschat.message FROM messageschat, utilisateurschat WHERE messageschat.ID_user = utilisateurschat.ID_user ]
+
+
+	*/
 }
 ?>
 	</section>
@@ -27,8 +49,11 @@ while (i machin) {
 	<section>
 		<form action="cible.php" method="post">
 			<label for="message">Votre message :</label>
-			<textarea type="text" name="message" id="message"></textarea>			
+			<textarea type="text" name="message" id="message required"></textarea>			
 			<br/>
+
+			<input type="hidden " id="datetime" value="yyyy_mm_dd_hh_mm_ss()" />				
+
 			<input type="submit" value="Envoyer message" />
 		</form>
 	</section>
@@ -49,7 +74,7 @@ Si le pseudo n'existe pas afficher le formulaire d'inscription
 	<section>
 		<form action="index.php" method="post">
 			<label for="pseudo">Votre pseudo :</label>
-			<input type="text" name="pseudo" id="psudo" />
+			<input type="text" name="pseudo" id="psudo" required />
 			<br/>
 			<input type="submit" value="Envoyer message" />
 		</form>
