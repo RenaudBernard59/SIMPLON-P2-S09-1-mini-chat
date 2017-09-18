29f680b649f75dfa659dd73460c91de4e23fbe0b
RenaudBernard59 <renaud.bernard@p59production.com>
2017-09-18 11:20:22 +0200

    PHP Skeleton of printing HTML webpage OK

--- index.php
+++ index.php
@@ -2,30 +2,33 @@
 <html lang=fr>
 <head>
 	<meta charset=utf-8>
-	<title>blah</title>
+	<title>Le petit chat</title>
 </head>
 <body>
 	<header><h1>Le petit chat</h1></header>
 	<main>
+<?php
+if (isset($_POST['pseudo'])) {
+/*====================================================================================
+//==========SECTION1
+Si le pseudo existe afficher Le chat [SECTION1]
+//====================================================================================*/
+?>
+	<p>Bonjour <?php echo $_POST['pseudo'];  ?></p>
 	<section>
-		<p>Hello</p>
-		<p>Hello</p>
-		<p>Hello</p>
-		<p>Hello</p>
-		<p>Hello</p>
-		<p>Hello</p>
+<?php
+while (i machin) {
+	echo "<p>" . [SQL] . "</p>";
+
+}
+?>
 	</section>
 	<hr/>
 	<section>
-		<form action="cible.php" methode="post">
-			<label for="pseudo">Votre pseudo :</label>
-			<input type="text" name="pseudo" id="psudo" />	
-	
-			<br/>
-
+		<form action="cible.php" method="post">
 			<label for="message">Votre message :</label>
 			<textarea type="text" name="message" id="message"></textarea>			
-
+			<br/>
 			<input type="submit" value="Envoyer message" />
 		</form>
 	</section>
@@ -33,3 +36,53 @@
 	<footer><small>&copy; 2017 - Renaud BERNARD</small></footer>
 </body>
 </html>
+<?php
+
+
+} else  {
+/*====================================================================================
+//==========SECTION1
+Si le pseudo n'existe pas afficher le formulaire d'inscription
+//====================================================================================*/
+?>
+	<p>Entrez votre pseudo !</p>
+	<section>
+		<form action="index.php" method="post">
+			<label for="pseudo">Votre pseudo :</label>
+			<input type="text" name="pseudo" id="psudo" />
+			<br/>
+			<input type="submit" value="Envoyer message" />
+		</form>
+	</section>
+	</main>
+	<footer><small>&copy; 2017 - Renaud BERNARD</small></footer>
+</body>
+</html>
+<?php
+}//END Conditionnal
+
+
+//====================================================================================
+//==========SECTION3
+//==============================:======================================================
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
