49638f5c610d30c966bce6737942d5820e0c51a6
RenaudBernard59 <renaud.bernard@p59production.com>
2017-09-19 10:33:45 +0200

    Editing and indenting the 2 PHP pages NOT Finish

--- chatmachine.php
+++ chatmachine.php
@@ -1,99 +1,97 @@
 <?php
 //Connexion SGBD//////////////////////////////////////////////////////////////////////////////
-try { $bdd = new PDO ('mysql:host=localhost;dbname=tp-minichat', 'root', 'monmotdepasse');}///
-catch (Exception $e) { die('Erreur : ' . $e->getMessage());}//////////////////////////////////
+try {
+                $bdd = new PDO('mysql:host=localhost;dbname=tp-minichat', 'root', 'monmotdepasse');
+} ///
+catch (Exception $e) {
+                die('Erreur : ' . $e->getMessage());
+} //////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-//Récupération des fonctions
-$erreur = "";
-$pseudo = htmlspecialchars($_POST['pseudo']);
+//Récupération des variables/fonctions
+$erreur  = "";
+$pseudo  = htmlspecialchars($_POST['pseudo']);
 $message = htmlspecialchars($_POST['message']);
-$date = htmlspecialchars($_POST['datetime']);
+$date    = htmlspecialchars($_POST['datetime']);
+function newUser($pseudo)
+{
+                $req = $bdd->prepare('INSERT INTO utilisateurschat(nomutilisateur) VALUES(:pseudo)');
+                $req->execute(array(
+                                'pseudo' => $pseudo
+                ));
+} //END newUser
+
+function addMessage($pseudo, $message, $date)
+{
+                $req = $bdd->prepare('INSERT INTO messageschat(ID_user, message, heure) VALUES( (SELECT ID_user FROM utilisateurschat WHERE nomutilisateur=:pseudo), :message, :date)');
+                $req->execute(array(
+                                'pseudo' => $pseudo,
+                                'message' => $message,
+                                'date' => $date
+                ));
+} //END addUser
+
 //Vérification du formulaire
 if (!empty($_POST['pseudo']) AND !empty($_POST['message']) AND !empty($_POST['datetime'])) {
-/*
------Form verifications script part1
-*/
-//Il n'y a pas de données nulles
- $pseudolength = strlen($pseudo);
-if ($pseudolength <= 100)  {
-/*
------pseudolength script part1
-*/
-//Rechercher si le pseudo existe
-$reqpseudo = $bdd->prepare("SELECT utilisateurschat.nomutilisateur FROM utilisateurschat WHERE nomutilisateur = ?");
-$reqpseudo->execute(array($pseudo));
-$pseudoexist = $reqpseudo->rowcount();
- if ($pseudoexist == 0) {
-/*
------pseudoexist script part1
-*/
-	// Le pseudo n'existe pas > on en crée un.
-		/////////////SCRIPT:::::::::::::::::
-echo $pseudo . " | ". $pseudoexist . " | ". $date . " | ". $message;
+                /*
+                -----Form verifications script part1
+                */
+                //Il n'y a pas de données nulles
+                $pseudolength = strlen($pseudo);
+                if ($pseudolength <= 100) {
+                                /*
+                                -----pseudolength script part1
+                                */
+                                //Rechercher si le pseudo existe
+                                $reqpseudo = $bdd->prepare("SELECT utilisateurschat.nomutilisateur FROM utilisateurschat WHERE nomutilisateur = ?");
+                                $reqpseudo->execute(array(
+                                                $pseudo
+                                ));
+                                $pseudoexist = $reqpseudo->rowcount();
+                                if ($pseudoexist == 0) {
+                                                /*
+                                                -----pseudoexist script part1
+                                                */
+                                                // Le pseudo n'existe pas > on en crée un.
+                                                /////////////SCRIPT:::::::::::::::::
+                                                echo $pseudo . " | " . $pseudoexist . " | " . $date . " | " . $message;
+                                } else {
+                                                /*
+                                                -----pseudoexist script part2
+                                                */
+                                                // Le pseudo existe > on l'utilise + pas de nouvelle entrée.
+                                                /////////////SCRIPT:::::::::::::::::
+                                                echo $pseudo . " | " . $pseudoexist . " | " . $date . " | " . $message;
+                                } //END pseudoexistden
+                } else {
+                                /*
+                                -----pseudolength script part2
+                                */
+                                
+                                $erreur = $erreur . " | Too long pseudo | ";
+                } // END pseudolength
+                
 } else {
-/*
------pseudoexist script part2
-*/
-	// Le pseudo existe > on l'utilise + pas de nouvelle entrée.
-		/////////////SCRIPT:::::::::::::::::
-echo $pseudo . " | ". $pseudoexist . " | ". $date . " | ". $message;
-} //END pseudoexist
-} else {
-/*
------pseudolength script part2
-*/
-
-$erreur = $erreur . " | Too long pseudo | ";
-} // END pseudolength
-
-} else {
-/*
------Form verifications script part2
-*/
-
-if (empty($_POST['pseudo'])) {
-	$erreur = $erreur . " | empty(pseudo)| ";
-}////
-if (empty($_POST['message'])) {
-	$erreur = $erreur . " | empty(message) | ";
-}////
-if ($_POST['datetime']) {
-	$erreur = $erreur . " | empty(date) | ";
-}////
+                /*
+                -----Form verifications script part2
+                */
+                
+                if (empty($_POST['pseudo'])) {
+                                $erreur = $erreur . " | empty(pseudo)| ";
+                } ////
+                if (empty($_POST['message'])) {
+                                $erreur = $erreur . " | empty(message) | ";
+                } ////
+                if ($_POST['datetime']) {
+                                $erreur = $erreur . " | empty(date) | ";
+                } ////
 } // END Form verifications
 
 
 
 
 
-///////////////////////////////////////////////////////////////////////////////////
-/* Block d'écriture SGBD
-//Ecriture dans la SGBD
-$req = $bdd->prepare('INSERT INTO utilisateurschat(nomutilisateur) VALUES(:nomutilisateur), messageschat(message, heure) VALUES(:message, :heure)');
-//Enregistrement des données
-$req->execute(array(
-	'nomutilisateur' => $_POST['pseudo'],
-	'ID_user' => $ID_user,
-	'message' => $$_POST['message'],
-	'heure' => $$_POST['datetime'],
-));
+//Return index
 header('Location:index.php');
-*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+///////////////////////////////////////////////////////////////////////////////////
--- index.php
+++ index.php
@@ -1,86 +1,101 @@
 <?php ////////////////////////////////////////PHP-STA///////////////////////////////////
 //Connexion SGBD//////////////////////////////////////////////////////////////////////////////
-try { $bdd = new PDO ('mysql:host=localhost;dbname=tp-minichat', 'root', 'monmotdepasse');}///
-catch (Exception $e) { die('Erreur : ' . $e->getMessage());}//////////////////////////////////
+try {
+                $bdd = new PDO('mysql:host=localhost;dbname=tp-minichat', 'root', 'monmotdepasse');
+} ///
+catch (Exception $e) {
+                die('Erreur : ' . $e->getMessage());
+} //////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////
 //Appel du contenu de la SGBD
 $reponse = $bdd->query('SELECT utilisateurschat.nomutilisateur, messageschat.message, messageschat.heure
 FROM messageschat, utilisateurschat
 WHERE messageschat.ID_user = utilisateurschat.ID_user');
 
-/*//////////////////////////////////////PHP-END/////////////////////////////////*/ ?>
+/*//////////////////////////////////////PHP-END/////////////////////////////////*/
+?>
 <!doctype html>
 <html lang=fr>
 <head>
-	<meta charset=utf-8>
-	<title>Le petit chat</title>
+        <meta charset=utf-8>
+        <title>Le petit chat</title>
 </head>
 <body>
-	<header><h1>Le petit chat</h1></header>
-	<main>
+        <header><h1>Le petit chat</h1></header>
+        <main>
 <?php ////////////////////////////////////////PHP-STA///////////////////////////////////
 //On vérifie le pseudo
 if (isset($_POST['pseudo'])) {
-$pseudo = htmlspecialchars($_POST['pseudo']);
-/*
------Pseudo verifications script part1
-*/
-/*====================================================================================
-//==========SECTION1
-Si le pseudo existe afficher Le chat [SECTION1]
-//====================================================================================*/
-////////////////////////////////////////PHP-END/////////////////////////////////// ?>
-	<p>Bonjour <?php echo $pseudo;  ?></p>
-	<section>
-		<?php
-			while ($donnees = $reponse->fetch()) {
-		?>
-			<p><strong> <?php echo $donnees['nomutilisateur'] . " | " . $donnees['heure']; ?></strong><br />
-			<?php echo $donnees['message']; ?></p>
-		<?php
-		}
-		?>
-	</section>
-	<hr/>
-	<p><?php if (isset($erreur)) 
-          {
-            echo $erreur ;
-          }  
-          ?></p>
-	<section>
-		<form action="chatmachine.php" method="post">
-			<label for="message">Votre message :</label>
-			<textarea type="text" name="message" id="message" required></textarea>			
-			<br/>
+                $pseudo = htmlspecialchars($_POST['pseudo']);
+                /*
+                -----Pseudo verifications script part1
+                */
+                /*====================================================================================
+                //==========SECTION1
+                Si le pseudo existe afficher Le chat [SECTION1]
+                //====================================================================================*/
+                ////////////////////////////////////////PHP-END/////////////////////////////////// 
+?>
+       <p>Bonjour <?php
+                echo $pseudo;
+?></p>
+        <section>
+                <?php
+                while ($donnees = $reponse->fetch()) {
+?>
+                       <p><strong> <?php
+                                echo $donnees['nomutilisateur'] . " | " . $donnees['heure'];
+?></strong><br />
+                        <?php
+                                echo $donnees['message'];
+?></p>
+                <?php
+                }
+?>
+       </section>
+        <hr/>
+        <p><?php
+                if (isset($erreur)) {
+                                echo $erreur;
+                }
+?></p>
+        <section>
+                <form action="chatmachine.php" method="post">
+                        <label for="message">Votre message :</label>
+                        <textarea type="text" name="message" id="message" required></textarea>            
+                        <br/>
 
-			<input type="hidden" name="datetime" id="datetime" />				
-			<input type="hidden" name="pseudo" id="pseudo" value="<?php echo $pseudo ;?>" />				
+                        <input type="hidden" name="datetime" id="datetime" />                
+                        <input type="hidden" name="pseudo" id="pseudo" value="<?php
+                echo $pseudo;
+?>" />                
 
-			<input type="submit" value="Envoyer message" />
-		</form>
-	</section>
+                        <input type="submit" value="Envoyer message" />
+                </form>
+        </section>
 <?php ////////////////////////////////////////PHP-STA///////////////////////////////////
 } else {
-/*
------Pseudo verifications script part2
-*/
-/*====================================================================================
-//==========SECTION2
-Si le pseudo n'existe pas afficher le formulaire d'inscription [SECTION2]
-//====================================================================================*/
-/*//////////////////////////////////////PHP-END/////////////////////////////////*/ ?>
-	<p>Entrez votre pseudo !</p>
-	<section>
-		<form action="index.php" method="post">
-			<label for="pseudo">Votre pseudo :</label>
-			<input type="text" name="pseudo" id="pseudo" required />
-			<br/>
-			<input type="submit" value="Envoyer message" />
-		</form>
-	</section>
+                /*
+                -----Pseudo verifications script part2
+                */
+                /*====================================================================================
+                //==========SECTION2
+                Si le pseudo n'existe pas afficher le formulaire d'inscription [SECTION2]
+                //====================================================================================*/
+                /*//////////////////////////////////////PHP-END/////////////////////////////////*/
+?>
+       <p>Entrez votre pseudo !</p>
+        <section>
+                <form action="index.php" method="post">
+                        <label for="pseudo">Votre pseudo :</label>
+                        <input type="text" name="pseudo" id="pseudo" required />
+                        <br/>
+                        <input type="submit" value="Envoyer message" />
+                </form>
+        </section>
 
 <?php ////////////////////////////////////////PHP-STA///////////////////////////////////
-}//END Pseudo verifications
+} //END Pseudo verifications
 
 
 //====================================================================================
@@ -88,25 +103,26 @@ Si le pseudo n'existe pas afficher le formulaire d'inscription [SECTION2]
 //==============================:======================================================
 
 
-/*//////////////////////////////////////PHP-END/////////////////////////////////*/ ?>
-	</main>
-	<footer><small>&copy; 2017 - Renaud BERNARD</small></footer>
-	<script>
+/*//////////////////////////////////////PHP-END/////////////////////////////////*/
+?>
+       </main>
+        <footer><small>&copy; 2017 - Renaud BERNARD</small></footer>
+        <script>
 
-		function js_yyyy_mm_dd_hh_mm_ss() {
-		  now = new Date();
-		  year = "" + now.getFullYear();
-		  month = "" + (now.getMonth() + 1); if (month.length == 1) { month = "0" + month; }
-		  day = "" + now.getDate(); if (day.length == 1) { day = "0" + day; }
-		  hour = "" + now.getHours(); if (hour.length == 1) { hour = "0" + hour; }
-		  minute = "" + now.getMinutes(); if (minute.length == 1) { minute = "0" + minute; }
-		  second = "" + now.getSeconds(); if (second.length == 1) { second = "0" + second; }
-		  return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
-		}
-		var setTime = js_yyyy_mm_dd_hh_mm_ss();
-		var dateForm = document.getElementById("datetime");
-		dateForm.setAttribute('value', setTime);
-	</script>
+                function js_yyyy_mm_dd_hh_mm_ss() {
+                now = new Date();
+                year = "" + now.getFullYear();
+                month = "" + (now.getMonth() + 1); if (month.length == 1) { month = "0" + month; }
+                day = "" + now.getDate(); if (day.length == 1) { day = "0" + day; }
+                hour = "" + now.getHours(); if (hour.length == 1) { hour = "0" + hour; }
+                minute = "" + now.getMinutes(); if (minute.length == 1) { minute = "0" + minute; }
+                second = "" + now.getSeconds(); if (second.length == 1) { second = "0" + second; }
+                return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+                }
+                var setTime = js_yyyy_mm_dd_hh_mm_ss();
+                var dateForm = document.getElementById("datetime");
+                dateForm.setAttribute('value', setTime);
+        </script>
 </body>
 </html>
 
@@ -125,3 +141,6 @@ Si le pseudo n'existe pas afficher le formulaire d'inscription [SECTION2]
 
 
 
+Download Formatting took: 238 ms
+PHP Formatter made by Spark Labs  
+Copyright Gerben van Veenendaal  
